18a1aadf4d4cea68f9fd6da20ae858479cf90dab
Erica Lin <ericalin.135@gmail.com>
2020-09-16 16:55:14 -0400

    Version using csvpath for heatmap data input

--- cluster-1.59/heatmap_clustering/demo.js
+++ cluster-1.59/heatmap_clustering/demo.js
@@ -1,13 +1,13 @@
 const addon = require('./build/Release/cclust');
 
 // Input
-var matrix = [["","col1","col2"],["row1",1,2],["row2",3,4],["row3",3,4]];
+var matrix_csvpath = "../../sample_data/smallGenesFile.csv";
 var distance_function = "e";
 var linkage_function = "a";
 var axes = "b";
 
 // Output after clustering
-var output_object =  addon.ccluster(matrix, distance_function, linkage_function, axes);
+var output_object =  addon.ccluster(matrix_csvpath, distance_function, linkage_function, axes);
 console.log(`\n The output is `, JSON.stringify(output_object));
 
 
